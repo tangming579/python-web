6a52a3fe7662613aaa3e75d9d240dd12ad2d952a
Tangming <infly123@sina.com>
2019-04-02 19:40:42 +0800

    Update day4_test.py

--- www/day4_test.py
+++ www/day4_test.py
@@ -3,8 +3,8 @@ import orm
 from models import User, Blog, Comment
 
 async def insert(loop):
-    await orm.create_pool(loop=loop, host='127.0.0.1', user='root', password='123456', db='awesome')
-    u = User(name='Test', email='test@example.com', passwd='123456', image='about:blank')
+    await orm.create_pool(loop=loop, host='localhost', user='root', password='123456', db='awesome')
+    u = User(name='Test', email='test2@example.com', passwd='123456', image='about:blank')
     await u.save()
 
 loop = asyncio.get_event_loop()
