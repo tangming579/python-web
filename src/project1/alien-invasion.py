f422d06afe26f3efce692559fd3b4d988f354521
Tangming <infly123@sina.com>
2019-06-25 19:03:44 +0800

    Update alien-invasion.py

--- src/project1/alien-invasion.py
+++ src/project1/alien-invasion.py
@@ -1,12 +1,15 @@
 import sys
 import pygame
 
+from settings import Settings
+
 def run_game():
 	#初始化游戏并创建屏幕对象
 	pygame.init()
-	screen = pygame.display.set_mode((1200,800))
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
-	bg_color = (230, 230, 230)
+	bg_color = ai_settings.bg_color
 	#游戏主循环
 	while True:		
 		#监听键盘和鼠标事件
